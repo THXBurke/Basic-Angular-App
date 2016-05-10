0deb57d23bc93b9669d48582536ee1eb57a7dcf5
THXBurke <rachelwheels@gmail.com>
2016-05-10 16:27:27 -0700

    fix css:dev

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const webpack = require('webpack-stream');
 gulp.task('webpack:dev', () => {
   gulp.src('app/js/entry.js')
   .pipe(webpack({
+    devtool: 'source-map',
     output: {
       filename: 'bundle.js'
     }
@@ -11,10 +12,15 @@ gulp.task('webpack:dev', () => {
   .pipe(gulp.dest('./build'));
 });
 
+gulp.task('css:dev', () => {
+  gulp.src('app/css/**/*.css')
+    .pipe(gulp.dest('./build'));
+}) ;
+
 gulp.task('static:dev', () => {
   gulp.src('app/**/*.html')
   .pipe(gulp.dest('./build'));
 });
 
-gulp.task('build:dev', ['webpack:dev', 'static:dev']);
+gulp.task('build:dev', ['webpack:dev', 'static:dev', 'css:dev']);
 gulp.task('default', ['build:dev']);
